53eb523855d55e431191660f6e7b2c4c73a62765
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-04-12 09:50:43 +0530

    Day20

---
+++ Day20/ExcelSheetDemo2/src/com/thekiranacademy/excelsheet/DemoMain.java
@@ -0,0 +1,15 @@
+package com.thekiranacademy.excelsheet;
+
+import com.thekiranacademy.config.DriverSetting;
+
+public class DemoMain extends DriverSetting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DemoMain obj=new DemoMain();
+		obj.readExcelSheet();
+		
+
+	}
+
+}
